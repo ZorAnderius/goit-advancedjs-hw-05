5c87f0326234fb4084e656dff27bf2939b42b44c
ZorAnderius <gabriel.zorander@gmail.com>
2024-06-20 14:18:32 +0100

    add math operations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { concatenation } from "./concatenation";
+import {
+    add,
+    multiply,
+    subtraction,
+    division,
+    remainder,
+    pow
+} from './sum'
 
 const button = document.querySelector('button');
 const input = document.querySelector('input');
@@ -8,3 +16,17 @@ if (button && input) {
         concatenation(input.value, 'hello');
     })
 }
+
+const n1 = 10;
+const n2 = 3;
+
+console.log(`n1=${n1},  n2=${n2},  ${n1} + ${n2}=`,add(n1, n2));
+console.log(`n1=${n1},  n2=${n2},  ${n1} * ${n2}=`,multiply(n1, n2));
+console.log(`n1=${n1},  n2=${n2},  ${n1} - ${n2}=`,subtraction(n1, n2));
+console.log(`n1=${n1},  n2=${n2},  ${n1} / ${n2}=`,division(n1, n2));
+console.log(`n1=${n1},  n2=${n2},  ${n1} % ${n2}=`,remainder(n1, n2));
+console.log(`n1=${n1},  n2=${n2},  ${n1} pow ${n2}=`,pow(n1, n2));
+console.log('Second number is 0')
+console.log(`n1=${n1},  n2=0,  ${n1} / 0=`,division(n1, 0));
+console.log(`n1=${n1},  n2=0,  ${n1} % 0=`, remainder(n1, 0));
+console.log(`n1=${n1},  n2=0,  ${n1} pow 0=`,pow(n1, 0));
---
+++ src/sum.ts
@@ -0,0 +1,13 @@
+export const add = (num1: number, num2: number) => num1 + num2;
+
+export const multiply = (num1: number, num2: number) => num1 * num2;
+
+export const subtraction = (num1: number, num2: number) => num1 - num2;
+
+export const division = (num1: number, num2: number) => num2 ? num1 / num2 : NaN;
+
+export const remainder = (num1: number, num2: number) => num2 ? num1 % num2 : NaN;
+
+export const pow = (num1: number, num2: number) => Math.pow(num1, num2);
+
+
